c82804406f5424bf376cbaf79b3d719310a0a99f
Mikhail Stepkin <mikhail.stepkin@gmail.com>
2018-03-13 16:05:20 +0300

    NSAttributedStringKey fixed

--- SwiftCharts/ChartUtils.swift
+++ SwiftCharts/ChartUtils.swift
@@ -10,26 +10,26 @@ import UIKit
 
 /// A collection of useful utilities for chart calculations
 open class ChartUtils {
-
+    
     /**
      Calculates the size of a string if it were drawn with a given font
-
+     
      - parameter text: The string to calculate size for
      - parameter font: The font used to calculate the drawn size
-
+     
      - returns: The size of the string if it were drawn with a given font
      */
     open class func textSize(_ text: String, font: UIFont) -> CGSize {
-        return NSAttributedString(string: text, attributes: [NSFontAttributeName: font]).size()
+        return NSAttributedString(string: text, attributes: [NSAttributedStringKey.font: font]).size()
     }
-
+    
     /**
      Calculates the bounding bounds of a string if drawn with a given font and rotated
-
+     
      - parameter text:  The string to calculate the bounds for
      - parameter font:  The font used to calculate the drawn bounds
      - parameter angle: The angle in degrees that the bounds should be rotated by
-
+     
      - returns: The bounding bounds for the string if drawn with a given font and rotated
      */
     open class func rotatedTextBounds(_ text: String, font: UIFont, angle: CGFloat) -> CGRect {
@@ -40,27 +40,28 @@ open class ChartUtils {
     
     /**
      Calculates the bounding rectangle of a rectangle after it's rotated.
-
+     
      Source: http://stackoverflow.com/a/9168238/930450
-
+     
      - parameter rect:    The original rectangle to rotate
      - parameter radians: The angle in radians that it's to be rotated
-
+     
      - returns: The bounding rectangle of the rotated rectangle
      */
     open class func boundingRectAfterRotatingRect(_ rect: CGRect, radians: CGFloat) -> CGRect {
         let xfrm = CGAffineTransform(rotationAngle: radians)
         return rect.applying(xfrm)
     }
-
+    
     /**
      Converts seconds to the same amount as a dispatch_time_t
-
+     
      - parameter secs: The number of seconds
-
+     
      - returns: The number of seconds as a dispatch_time_t
      */
     open class func toDispatchTime(_ secs: Float) -> DispatchTime {
         return DispatchTime.now() + TimeInterval(secs)
     }
 }
+
